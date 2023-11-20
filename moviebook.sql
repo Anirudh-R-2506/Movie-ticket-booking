fa57f12f079f54e8a3c04db9d50ef10633882760
Anirudh R <anirudhnfs01@gmail.com>
2023-11-20 07:33:38 +0530

    New beginnings

--- moviebook.sql
+++ moviebook.sql
@@ -1,5 +1,33 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Nov 20, 2023 at 02:02 AM
+-- Server version: 5.7.39
+-- PHP Version: 8.2.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `moviebook`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `add_movie`
+--
+
+DROP TABLE IF EXISTS `add_movie`;
 CREATE TABLE `add_movie` (
   `id` int(25) NOT NULL,
   `movie_name` varchar(100) NOT NULL,
@@ -34,6 +62,7 @@ INSERT INTO `add_movie` (`id`, `movie_name`, `directer`, `release_date`, `categr
 -- Table structure for table `admin`
 --
 
+DROP TABLE IF EXISTS `admin`;
 CREATE TABLE `admin` (
   `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
@@ -49,13 +78,13 @@ CREATE TABLE `admin` (
 INSERT INTO `admin` (`id`, `name`, `email`, `password`, `is_active`) VALUES
 (1, 'Jainam', 'jainmdg@gmail.com', 'admin', '1');
 
-
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `customers`
 --
 
+DROP TABLE IF EXISTS `customers`;
 CREATE TABLE `customers` (
   `id` int(10) NOT NULL,
   `uid` int(10) NOT NULL,
@@ -82,7 +111,49 @@ INSERT INTO `customers` (`id`, `uid`, `movie`, `show_time`, `seat`, `totalseat`,
 (2, 1, 'Tanaji', '15:15', 'F7,F8,E7,E8,A7,A8', '6', '1200', 'Thu-09-21 ', 'Fri-09-21 ', 'pratik d', '145260', '2021-09-30', 349, 1890244096),
 (3, 2, 'Chaal Jeevi Laiye', '15:00', 'I5,I6,H5,H6,G5,G6', '6', '600', 'Thu-09-21 ', 'Fri-09-21 ', 'parthiv', '45456845565', '2021-10-23', 455, 560041981),
 (4, 3, 'Chaal Jeevi Laiye', '15:00', 'I7,I8', '2', '200', 'Thu-09-21 ', 'Fri-10-21 ', 'rushabh', '545656', '2021-09-29', 545, 447698228),
-(5, 3, 'Chaal Jeevi Laiye', '18:15', 'G9', '1', '100', 'Thu-09-21 ', 'Fri-10-21 ', 'rushabh', '565464', '2021-09-10', 655, 2080652377);
+(5, 3, 'Chaal Jeevi Laiye', '18:15', 'G9', '1', '100', 'Thu-09-21 ', 'Fri-10-21 ', 'rushabh', '565464', '2021-09-10', 655, 2080652377),
+(6, 6, 'Avengers', '21:00', 'I7,I8,I9', '3', '300', 'Sun-11-23 ', 'Mon-11-23 ', 'Test usr', '12345678910', '2023-12-01', 123, 1034859511),
+(8, 6, 'Avengers', '21:00', '', '2', '200', 'Mon-11-23 ', 'Tue-11-23 ', 'aksdcnkjac jancanscn', '12342435325325', '2023-12-01', 123, 225504038);
+
+--
+-- Triggers `customers`
+--
+DROP TRIGGER IF EXISTS `Seat_check`;
+DELIMITER $$
+CREATE TRIGGER `Seat_check` BEFORE INSERT ON `customers` FOR EACH ROW BEGIN
+	DECLARE seat_exists INT;
+    DECLARE seat VARCHAR(100);
+
+    -- Split the new seat combination into individual seats
+    CREATE TEMPORARY TABLE temp_seats (seat VARCHAR(100));
+    SET seat = SUBSTRING_INDEX(NEW.seat, ',', 1);
+    WHILE LENGTH(seat) > 0 DO
+        INSERT INTO temp_seats VALUES (seat);
+        SET NEW.seat = SUBSTRING(NEW.seat, LENGTH(seat) + 2);
+        SET seat = SUBSTRING_INDEX(NEW.seat, ',', 1);
+    END WHILE;
+
+    -- Check if any seat in the new combination already exists in the existing bookings
+    SELECT COUNT(*)
+    INTO seat_exists
+    FROM temp_seats ts
+    WHERE EXISTS (
+        SELECT 1
+        FROM customers b
+        WHERE FIND_IN_SET(ts.seat, b.seat) > 0
+    );
+
+    -- If seat_exists is greater than 0, it means one or more seats are already booked
+    IF seat_exists > 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'One or more seats are already booked';
+    END IF;
+
+    -- Drop the temporary table
+    DROP TEMPORARY TABLE IF EXISTS temp_seats;
+END
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -90,6 +161,7 @@ INSERT INTO `customers` (`id`, `uid`, `movie`, `show_time`, `seat`, `totalseat`,
 -- Table structure for table `feedback`
 --
 
+DROP TABLE IF EXISTS `feedback`;
 CREATE TABLE `feedback` (
   `id` int(100) NOT NULL,
   `name` varchar(100) NOT NULL,
@@ -110,9 +182,32 @@ INSERT INTO `feedback` (`id`, `name`, `email`, `massage`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `seat_lock`
+--
+
+DROP TABLE IF EXISTS `seat_lock`;
+CREATE TABLE `seat_lock` (
+  `movie` varchar(100) NOT NULL,
+  `show_time` varchar(100) NOT NULL,
+  `seat` varchar(100) NOT NULL,
+  `lock_until` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `seat_lock`
+--
+
+INSERT INTO `seat_lock` (`movie`, `show_time`, `seat`, `lock_until`) VALUES
+('Avengers', '21:00', 'I10', '2023-11-20 02:05:04'),
+('Avengers', '21:00', 'I11', '2023-11-20 02:05:04');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `theater_show`
 --
 
+DROP TABLE IF EXISTS `theater_show`;
 CREATE TABLE `theater_show` (
   `id` int(25) NOT NULL,
   `show` varchar(100) NOT NULL,
@@ -137,11 +232,12 @@ INSERT INTO `theater_show` (`id`, `show`, `theater`) VALUES
 -- Table structure for table `user`
 --
 
+DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` int(25) NOT NULL,
   `username` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `mobile` int(10) NOT NULL,
+  `mobile` varchar(100) NOT NULL,
   `city` varchar(25) NOT NULL,
   `password` varchar(12) NOT NULL,
   `image` varchar(100) NOT NULL
@@ -152,11 +248,12 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `username`, `email`, `mobile`, `city`, `password`, `image`) VALUES
-(1, 'pratik', 'prati@gmail.com', 4578945612, 'Surendranagar', '4550', ''),
-(2, 'parthiv', 'parthi@gmail.com', 2147483647, 'WADHWAN', '78963', ''),
-(3, 'rushabh', 'rushab@gmail.com', 1198875215, 'joravarnagar', '147852', ''),
-(4, 'hetanshi', 'hetansh@gmail.com', 1234567890, 'fggfrg', '0', ''),
-(5, 'dharmin', 'dharm@gmail.com', 2147483647, 'surat', 'hitesh', 'eye candy wallpapers 6 by deadpxl.jpg');
+(1, 'pratik', 'prati@gmail.com', '4578945612', 'Surendranagar', '4550', ''),
+(2, 'parthiv', 'parthi@gmail.com', '2147483647', 'WADHWAN', '78963', ''),
+(3, 'rushabh', 'rushab@gmail.com', '1198875215', 'joravarnagar', '147852', ''),
+(4, 'hetanshi', 'hetansh@gmail.com', '1234567890', 'fggfrg', '0', ''),
+(5, 'dharmin', 'dharm@gmail.com', '2147483647', 'surat', 'hitesh', 'eye candy wallpapers 6 by deadpxl.jpg'),
+(6, 'Test User', 'anirudhnfs01@gmail.com', '9361956013', 'Chennai', 'password', '');
 
 --
 -- Indexes for dumped tables
@@ -179,13 +276,20 @@ ALTER TABLE `admin`
 -- Indexes for table `customers`
 --
 ALTER TABLE `customers`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_index` (`movie`,`show_time`,`seat`);
 
 --
 -- Indexes for table `feedback`
 --
 ALTER TABLE `feedback`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `seat_lock`
+--
+ALTER TABLE `seat_lock`
+  ADD UNIQUE KEY `unique_index` (`movie`,`show_time`,`seat`);
 
 --
 -- Indexes for table `theater_show`
@@ -219,7 +323,7 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `feedback`
@@ -237,7 +341,18 @@ ALTER TABLE `theater_show`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+DELIMITER $$
+--
+-- Events
+--
+DROP EVENT IF EXISTS `delete_expired`$$
+CREATE DEFINER=`root`@`localhost` EVENT `delete_expired` ON SCHEDULE EVERY 1 HOUR STARTS '2023-11-20 06:45:28' ON COMPLETION NOT PRESERVE ENABLE DO BEGIN
+	DELETE FROM seat_lock WHERE lock_until <= NOW();
+END$$
+
+DELIMITER ;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
